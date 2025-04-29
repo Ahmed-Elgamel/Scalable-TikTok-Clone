40b54545091fd7c26da2b8d3e5a5cd4263630a14
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2025-04-29 21:59:44 +0300

    CREATE news feed microservice

---
+++ News-Feed-Service/src/test/java/com/example/News/Feed/Service/NewsFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.News.Feed.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
