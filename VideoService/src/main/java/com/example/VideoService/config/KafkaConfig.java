13ca030bded31b25e44dd18637415cdb7708e643
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2025-04-30 19:42:44 +0300

    ADD configurations and fixes for publishing messages

--- VideoService/src/main/java/com/example/VideoService/config/KafkaConfig.java
+++ VideoService/src/main/java/com/example/VideoService/config/KafkaConfig.java
@@ -15,7 +15,7 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // serialize the ket into byes
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // serialize the value into bytes
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
