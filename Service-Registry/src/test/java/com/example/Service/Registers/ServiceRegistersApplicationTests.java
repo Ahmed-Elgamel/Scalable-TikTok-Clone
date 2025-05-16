b2687433495a1ce8d8226a899ac7ecab07069124
Ahmed Wael <127795744+mellegyy@users.noreply.github.com>
2025-05-16 23:07:24 +0300

    service registry

---
+++ Service-Registry/src/test/java/com/example/Service/Registers/ServiceRegistersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Service.Registers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
