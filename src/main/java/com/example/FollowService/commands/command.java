00d492cbd855fd7afd17114632e7d2145e1b0542
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2025-05-17 17:08:22 +0300

    CLEAN UP

--- src/main/java/com/example/FollowService/commands/command.java
+++
@@ -1,7 +0,0 @@
-package com.example.FollowService.commands;
-
-import com.example.FollowService.Model.Follow;
-
-public interface command {
-      Follow execute();
-}
--- src/main/java/com/example/FollowService/strategy/Filter.java
+++
@@ -1,9 +0,0 @@
-package com.example.FollowService.strategy;
-
-import com.example.FollowService.Model.Follow;
-
-import java.util.List;
-
-public interface Filter {
-    List<String> filter(List<Follow> followers);
-}
