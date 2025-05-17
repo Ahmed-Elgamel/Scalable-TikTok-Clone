3dbf0c5501c84bda30913e00964aa1b2408c9c9d
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2025-05-17 17:02:04 +0300

    Encapsualte follow service in a folder

---
+++ FollowService/src/main/java/com/example/FollowService/Repository/FollowRepository.java
@@ -0,0 +1,12 @@
+package com.example.FollowService.Repository;
+
+import com.example.FollowService.Model.Follow;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowRepository extends MongoRepository<Follow, String> {
+    List<Follow> findByFolloweeId(String followeeId);
+    Optional<Follow> findByFollowerIdAndFolloweeId(String followerId, String followeeId);
+}
